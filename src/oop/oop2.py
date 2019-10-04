ab7fbe938f4991ce922d93e2f1186b1193e64fbf
Hunter Raffety <hsraffety@gmail.com>
2019-10-04 08:37:06 -0700

    returning vroom instead of printing

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -8,7 +8,7 @@ class GroundVehicle():
         self.num_wheels = num_wheels
 
     def drive(self):
-        print(f"vroooom")
+        return "vroooom"
     # TODO
 
 
@@ -23,7 +23,7 @@ class Motorcycle(GroundVehicle):
         self.num_wheels = num_wheels
 
     def drive(self):
-        print(f"BRAAAP!!")
+        return "BRAAAP!!"
 
 # TODO
 
